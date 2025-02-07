279613abefd0d5809a0bfbe6e978e41ee473d864
Kao197 <yuntse197@gmail.com>
2025-02-07 16:59:56 +0800

    day5

--- pages/class5-3.py
+++ pages/class5-3.py
@@ -11,17 +11,17 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 st.title("猜迷小游戲")
 
-if "history" not in st.session_state:
-    st.session_state.history = []
+if "history22" not in st.session_state:
+    st.session_state.history2 = []
 
-for message in st.session_state.history:
+for message in st.session_state.history2:
     if message["role"] != "developer":
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
 message = st.chat_input("請輸入訊息")  # 聊天輸入框元件
 if message:
-    st.session_state.history.append({"role": "user", "content": message})
+    st.session_state.history2.append({"role": "user", "content": message})
     completion = client.chat.completions.create(
         model="gpt-4o",
         temperature=1,
@@ -37,9 +37,9 @@ if message:
                     """,
             },
         ]
-        + st.session_state.history,
+        + st.session_state.history2,
     )
-    st.session_state.history.append(
+    st.session_state.history2.append(
         {"role": "assistant", "content": completion.choices[0].message.content}
     )
     st.rerun()
