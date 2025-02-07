2492314e43bde223ecf8eb33b35e4eaca1a09bd9
Kao197 <yuntse197@gmail.com>
2025-02-07 13:27:11 +0800

    day4

--- pages/class2-3.py
+++ pages/class2-3.py
@@ -10,11 +10,11 @@ x = st.session_state.ans
 st.title("猜數字小游戲")
 e = st.number_input("請輸入數字", min_value=0, max_value=100, step=1, value=0)
 st.write(f"你還有 {n} 次機會")
-if n == 0:
-    st.write(f"gg答案是{x}")
-elif int(e) == x:
+if int(e) == x:
     st.balloons()
     st.write("答對!!!!!")
+elif n == 0:
+    st.write(f"gg答案是{x}")
 elif int(e) < x:
     st.write("再大一點!!")
     st.session_state.number = st.session_state.number - 1
