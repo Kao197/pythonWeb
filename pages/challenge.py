e57ae5ad775f3a472d7c11d15645c1f88a7f7479
Kao197 <yuntse197@gmail.com>
2025-02-07 13:36:23 +0800

    day5

--- pages/challenge.py
+++ pages/challenge.py
@@ -14,23 +14,24 @@ x = st.session_state.ans
 st.title("猜數字小游戲")
 e = st.number_input("請輸入數字", min_value=0, max_value=100, step=1, value=0)
 st.write(f"你還有 {n} 次機會")
-if int(e) == x:
+
+
+st.write(f"小於{st.session_state.number2}大於{st.session_state.number3}")
+if n == 0:
+    st.write(f"gg答案是{x}")
+elif int(e) == x:
     st.balloons()
     st.write("答對!!!!!")
-elif n == 0:
-    st.write(f"gg答案是{x}")
 elif int(e) < x:
     st.write("再大一點!!")
     st.session_state.number = st.session_state.number - 1
     if st.session_state.number3 < e:
         st.session_state.number3 = e
-    st.write(f"小於{st.session_state.number2}大於{st.session_state.number3}")
 elif int(e) > x:
     st.write("再小一點!!")
     st.session_state.number = st.session_state.number - 1
     if st.session_state.number2 > e:
         st.session_state.number2 = e
-    st.write(f"小於{st.session_state.number2}大於{st.session_state.number3}")
 if st.button("reset"):
     st.session_state.ans = r.randrange(0, 101)
     st.session_state.number2 = 100
