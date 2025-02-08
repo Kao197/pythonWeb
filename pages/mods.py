7197b14c001978a2528da3d3eb34839554e0e972
Kao197 <yuntse197@gmail.com>
2025-02-08 11:49:07 +0800

    day6

---
+++ pages/mods.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from openai import OpenAI
+import dotenv  # pip install python-dotenv -U
+import os
+import menu
+
+menu.menu()
+dotenv.load_dotenv()
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+st.title("寫程式AI")
+
+if "history" not in st.session_state:
+    st.session_state.history = []
+
+for message in st.session_state.history:
+    if message["role"] != "developer":
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+message = st.chat_input("請輸入訊息")  # 聊天輸入框元件
+if message:
+    st.session_state.history.append({"role": "user", "content": message})
+    completion = client.chat.completions.create(
+        model="gpt-4o",
+        temperature=0,
+        messages=[
+            {
+                "role": "developer",
+                "content": "你是一個寫程式機器人，只能用繁體中文(#zh-TW)和日文回應後續對話。",
+            },
+        ]
+        + st.session_state.history,
+    )
+    st.session_state.history.append(
+        {"role": "assistant", "content": completion.choices[0].message.content}
+    )
+    st.rerun()
