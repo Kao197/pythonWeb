c7ec31e3cf50fc6800ee756a209f2efe7b3f74a5
Kao197 <yuntse197@gmail.com>
2025-02-12 22:30:32 +0800

    a

--- pages/mods.py
+++ pages/mods.py
@@ -11,17 +11,17 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 st.title("寫程式AI")
 
-if "history" not in st.session_state:
-    st.session_state.history = []
+if "history3" not in st.session_state:
+    st.session_state.history3 = []
 
-for message in st.session_state.history:
+for message in st.session_state.history3:
     if message["role"] != "developer":
         with st.chat_message(message["role"]):
             st.write(message["content"])
 
 message = st.chat_input("請輸入訊息")  # 聊天輸入框元件
 if message:
-    st.session_state.history.append({"role": "user", "content": message})
+    st.session_state.history3.append({"role": "user", "content": message})
     completion = client.chat.completions.create(
         model="gpt-4o",
         temperature=0,
@@ -31,9 +31,9 @@ if message:
                 "content": "你是一個寫程式機器人，只能用繁體中文(#zh-TW)和日文回應後續對話。",
             },
         ]
-        + st.session_state.history,
+        + st.session_state.history3,
     )
-    st.session_state.history.append(
+    st.session_state.history3.append(
         {"role": "assistant", "content": completion.choices[0].message.content}
     )
     st.rerun()
