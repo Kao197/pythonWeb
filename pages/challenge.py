f5d479142232282e17eb19463cacebb642b35c40
Kao197 <yuntse197@gmail.com>
2025-02-19 09:24:53 +0800

    修改

--- pages/challenge.py
+++ pages/challenge.py
@@ -4,14 +4,14 @@ import menu
 
 menu.menu()
 
-if "ans" not in st.session_state:
-    st.session_state.ans = r.randrange(0, 101)
+if "answer" not in st.session_state:
+    st.session_state.answer = r.randrange(0, 101)
 if "number2" not in st.session_state:
     st.session_state.number2 = 100
 if "number3" not in st.session_state:
     st.session_state.number3 = 0
 if "number" not in st.session_state:
-    st.session_state.number = 8
+    st.session_state.number4 = 8
 
 n = st.session_state.number
 x = st.session_state.ans
@@ -24,19 +24,19 @@ elif int(e) == x:
     st.write("答對!!!!!")
 elif int(e) < x:
     st.write("再大一點!!")
-    st.session_state.number = st.session_state.number - 1
+    st.session_state.number4 = st.session_state.number4 - 1
     if st.session_state.number3 < e:
         st.session_state.number3 = e
 elif int(e) > x:
     st.write("再小一點!!")
-    st.session_state.number = st.session_state.number - 1
+    st.session_state.number4 = st.session_state.number4 - 1
     if st.session_state.number2 > e:
         st.session_state.number2 = e
 st.write(f"小於{st.session_state.number2}大於{st.session_state.number3}")
-st.write(f"你還有 {st.session_state.number} 次機會")
+st.write(f"你還有 {st.session_state.number4} 次機會")
 if st.button("reset"):
     st.session_state.ans = r.randrange(0, 101)
     st.session_state.number2 = 100
     st.session_state.number3 = 0
-    st.session_state.number = 8
+    st.session_state.number4 = 8
     st.rerun()
